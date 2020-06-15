031752b89a261b9b98a4ef681b539df65ddad579
Lam Nguyen <lam.nguyen@bstarsolutions.com>
2020-06-15 20:06:36 +0700

    format code

--- libs.py
+++ libs.py
@@ -1,16 +1,21 @@
 import re
 
+
 def convert_to_dict(keys, value):
     return dict(zip(keys, value))
 
+
 def get_digit_number_in_string(string):
     digit = [int(i) for i in string.split() if i.isdigit()]
-    digitNum = str(digit)
-    s = digitNum.replace('[', '')
+    digit_num = str(digit)
+    s = digit_num.replace('[', '')
     g = s.replace(']', '')
     return g
+
+
 def extract_number(string):
     return (re.findall('\d+', string))[0]
+
 
 def get_variable_type(variable):
     v = type(variable)
